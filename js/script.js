4e4303966466ccfc88f8c1702557abd893cb34af
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-04-24 16:39:27 +0000

    added stuff

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Adrina peighambarzadeh All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Adrina peighambarzadeh
+
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
+const randomNumber = Math.floor(Math.random() * 6) + 1
+/**
+* This function generates random positive or negative number
+*/
+// eslint-disable-next-line no-unused-vars
+
+function generateNumber () {
+
+ // input
+const optionPositive = document.getElementById('option-positive').checked
+
+ // process
+if (optionPositive === true) {
+
+   // output
+document.getElementById('answer').innerHTML =
+'Your random number is: ' + randomNumber
+} else {
+
+   // process
+   const randomNegativeNumber = randomNumber * -1
+   // output
+document.getElementById('answer').innerHTML =
+'Your random number is: ' + randomNegativeNumber
+}
+
+}
